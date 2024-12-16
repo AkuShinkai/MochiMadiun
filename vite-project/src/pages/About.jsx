a8cdf5e21f57915b638bb60815eeebb644aac165
Fariz <farizbakur@gmail.com>
2024-12-16 19:02:21 +0700

    update

--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import useCustomJS from "../useCostumeJS";
 import axiosClient from "../axios-client";
-import about from "../assets/Android WIth Github.png";
+import about from "../assets/About.jpg";
 
 function About() {
     useCustomJS();
